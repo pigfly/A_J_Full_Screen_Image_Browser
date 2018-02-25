3d9324ca77ced85b4200b1405abad2800ac79b9d
Alex Jiang <pigflyjky@gmail.com>
2018-02-25 12:59:08 +1100

    init

---
+++ A_J_Full_Screen_Image_Browser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  A_J_Full_Screen_Image_Browser
+//
+//  Created by Junliang Jiang on 25/2/18.
+//  Copyright © 2018 Junliang Jiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
